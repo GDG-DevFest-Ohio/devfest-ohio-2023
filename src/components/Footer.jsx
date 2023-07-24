0592c84d88d120bfd735b59b2766606d30ae99c7
Greg Miller <hello@gregmiller.io>
2023-07-24 13:02:09 -0400

    Update Footer.jsx
    - Add branding
    - Update copyright

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,12 +4,20 @@ import { Logo } from '@/components/Logo'
 export function Footer() {
   return (
     <footer className="flex-none py-16">
-      <Container className="flex flex-col items-center justify-between md:flex-row">
-        <Logo className="h-12 w-auto text-slate-900" />
-        <p className="mt-6 text-base text-slate-500 md:mt-0">
-          Copyright &copy; {new Date().getFullYear()} DeceptiConf, LLC. All
-          rights reserved.
-        </p>
+      <Container className="flex flex-col items-end justify-between md:flex-row">
+        <div className="flex flex-col justify-center">
+          <Logo className="w-auto h-20 text-slate-900" />
+          <p className='text'>GDG Ohio Devfest 2023</p>
+        </div>
+        <div className='flex flex-col items-end justify-between md:flex-row'>
+          
+        
+        
+          <p className="mt-6 text-base text-slate-500 md:mt-0">
+            Copyright &copy; {new Date().getFullYear()} Shrinkray Labs. All
+            rights reserved.
+            </p>
+        </div>
       </Container>
     </footer>
   )
