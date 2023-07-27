82fecb9415b19ec1145b5dfd86454dc49c51c0e3
Greg Miller <hello@gregmiller.io>
2023-07-27 13:26:19 -0400

    Change color to fix accessibility

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,9 +2,9 @@ import Head from 'next/head'
 
 import { Hero } from '@/components/Hero'
 import { Newsletter } from '@/components/Newsletter'
-import { Schedule } from '@/components/Schedule'
-import { Speakers } from '@/components/Speakers'
-import { Sponsors } from '@/components/Sponsors'
+// import { Schedule } from '@/components/Schedule'
+// import { Speakers } from '@/components/Speakers'
+// import { Sponsors } from '@/components/Sponsors'
 
 export default function Home() {
   return (
