bae38dae3f50449eb9d46514845effb633d9b8ba
Greg Miller <hello@gregmiller.io>
2023-07-28 22:53:32 -0400

    Refactor

--- src/components/TicketButton.jsx
+++ src/components/TicketButton.jsx
@@ -2,8 +2,8 @@ import { Button } from '@/components/Button'
 
 export function TicketButton({ path, content }) {
   return (
-    <div className="flex justify-center w-full mt-10 sm:w-auto sm:justify-start lg:grow lg:basis-0">
-      <Button href={ path }>{ content }</Button>
+    <div className="mt-10 flex w-full justify-center sm:w-auto sm:justify-start lg:grow lg:basis-0">
+      <Button href={path}>{content}</Button>
     </div>
   )
 }
