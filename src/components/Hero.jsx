a0b231f8bcc57f3705aa342339ea337acfff1d23
Greg Miller <hello@gregmiller.io>
2023-07-28 22:54:59 -0400

    Improve content
    - Add descriptive info to exaplain each page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,12 +7,12 @@ export function Hero() {
     <div className="relative py-20 sm:pb-24 sm:pt-36">
       <BackgroundImage className="-bottom-14 -top-36" />
       <Container className="relative">
-        <div className="max-w-2xl mx-auto lg:max-w-4xl lg:px-12">
-          <h1 className="text-5xl font-bold tracking-tighter text-blue-600 font-display sm:text-7xl">
+        <div className="mx-auto max-w-2xl lg:max-w-4xl lg:px-12">
+          <h1 className="font-display text-5xl font-bold tracking-tighter text-blue-600 sm:text-7xl">
             <span className="sr-only">Ohio Devfest - </span>Welcome to the
             largest Ohio Google conference!
           </h1>
-          <div className="mt-6 space-y-6 text-2xl tracking-tight text-blue-900 font-display">
+          <div className="mt-6 space-y-6 font-display text-2xl tracking-tight text-blue-900">
             <p>
               <span className="font-bold">Discover Ohio DevFest.</span> Hosted
               by Google Developer Groups from Ohio tech strongholds. Enjoy
@@ -31,15 +31,15 @@ export function Hero() {
               from Googlers, Google Developer Experts and industry
               professionals. In 2022, GDGs ran 550+ DevFests, helping 320k+
               developers.{' '}
-              <span className="italic font-bold">
+              <span className="font-bold italic">
                 Come to learn, share, and connect with the community.{' '}
               </span>
             </p>
           </div>
-          <div className="flex justify-center w-full mt-10 sm:w-auto sm:justify-start lg:grow lg:basis-0">
+          <div className="mt-10 flex w-full justify-center sm:w-auto sm:justify-start lg:grow lg:basis-0">
             <TicketButton path="/get-tickets" content="Get your tickets" />
           </div>
-          <dl className="grid grid-cols-2 mt-10 gap-x-10 gap-y-6 sm:mt-16 sm:gap-x-16 sm:gap-y-10 sm:text-center lg:auto-cols-auto lg:grid-flow-col lg:grid-cols-none lg:justify-start lg:text-left">
+          <dl className="mt-10 grid grid-cols-2 gap-x-10 gap-y-6 sm:mt-16 sm:gap-x-16 sm:gap-y-10 sm:text-center lg:auto-cols-auto lg:grid-flow-col lg:grid-cols-none lg:justify-start lg:text-left">
             {[
               ['Speakers', 'TBD'],
               ['People Attending', '300+'],
