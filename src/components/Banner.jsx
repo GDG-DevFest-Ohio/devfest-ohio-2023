fc6066df07ad8927a122618a35262594fbd98eb4
Greg Miller <hello@gregmiller.io>
2023-08-04 17:42:52 -0400

    Update Banner.jsx
    - Use CalendarButton

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,29 +1,21 @@
-import { StarIcon, CalendarDaysIcon } from '@heroicons/react/20/solid'
+import { AddToCalendarButton } from 'add-to-calendar-button-react'
 
 export default function Banner() {
   return (
-    <div className="flex items-center justify-center gap-x-6 rounded-lg bg-blue-600 px-4 py-2.5 text-center sm:px-3.5  sm:px-6 ">
-      <p className="text-lg leading-6 text-white">
-        <StarIcon
-          className="mx-1 mb-2 inline h-4 w-4 fill-current"
-          aria-hidden="true"
-        />
-        <strong className="font-semibold sm:text-2xl">Google DevFest</strong>
-        <StarIcon
-          className="whitespace-wrap mx-1 mb-2 inline h-4 w-4 fill-current"
-          aria-hidden="true"
-        />
-        <strong className="whitespace whitespace-nowrap sm:text-lg">
-          <span className="">Join us in</span> Bowling&nbsp;Green
-        </strong>
-        <strong className="sm:text-lg">
-          <CalendarDaysIcon
-            className="mx-1 mb-2 me-1 ms-4 inline h-6 w-6 whitespace-nowrap fill-current sm:ms-2"
-            aria-hidden="true"
-          />
-          November 11 – 12{' '}
-        </strong>
-      </p>
+    <div className="flex items-center justify-center gap-x-6 rounded-lg  px-4 py-2.5 text-center sm:px-3.5  sm:px-6 ">
+      <AddToCalendarButton
+        name="Google DevFest 2023"
+        description="The 2023 Ohio DevFest is a local tech conference & hackathon hosted by Ohio-based Google Developer Groups (GDGs). It features talks, hands-on demos, workshops, and codelabs on the latest Google tech, plus featured talks intended to broaden interest and appeal."
+        startDate="2023-11-11"
+        endDate="2023-11-12"
+        timezpone="America/New_York"
+        location="BGSU, Bowling Green, Ohio"
+        availability="Busy"
+        options={['Apple', 'Google', 'Yahoo', 'iCal']}
+        buttonStyle="flat"
+        trigger="click"
+        lightMode="bodyScheme"
+      ></AddToCalendarButton>
     </div>
   )
 }
