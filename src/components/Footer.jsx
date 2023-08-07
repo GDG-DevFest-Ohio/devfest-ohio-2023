da30d987c3d210cce0552aa173001722f8d64fe0
Greg Miller <hello@gregmiller.io>
2023-08-07 15:43:06 -0400

    Update Footer.jsx
    - Change image attachment

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import { Container } from '@/components/Container'
-import { Logo } from '@/components/Logo'
+import { Logo } from '@/components/LogoFooter'
 import CalendarButton from '@/components/CalendarButton'
 
 export function Footer() {
@@ -10,10 +10,10 @@ export function Footer() {
       </div>
       <Container className="flex flex-row">
         <div className="flex">
-          <Logo className="h-20 w-auto text-blue-600" />
+          <Logo className="w-auto h-20 text-blue-600" />
         </div>
 
-        <div className="mx-auto flex flex-row items-end text-center">
+        <div className="flex flex-row items-end mx-auto text-center">
           <p className="mt-6 text-base text-slate-500 md:mt-0">
             Copyright &copy; {new Date().getFullYear()} Shrinkray Labs. All
             rights reserved.
