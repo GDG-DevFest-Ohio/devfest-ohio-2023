9bc5540ae82b9c4fb6c7dc600ba68b6c0c28d4fb
Greg Miller <hello@gregmiller.io>
2023-08-08 10:45:19 -0400

    Update Header.jsx
    - Deploy fix with no SSR method
    - Solves hydration issues

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,12 @@
+import Link from 'next/link'
+import dynamic from 'next/dynamic'
 import { Container } from '@/components/Container'
 import { DiamondIcon } from '@/components/DiamondIcon'
-import Link from 'next/link'
 import { Logo } from '@/components/Logo'
-import CalendarButton from '@/components/CalendarButton'
+const CalendarButtonWithNoSSR = dynamic(
+  () => import('@/components/CalendarButton'),
+  { ssr: false },
+)
 
 export function Header() {
   return (
@@ -10,11 +14,11 @@ export function Header() {
       <Container className="flex flex-wrap items-center justify-center sm:justify-between lg:flex-nowrap">
         <div className="mt-10 lg:mt-0 lg:grow lg:basis-0">
           <Link href="/">
-            <Logo className="w-auto h-24 text-blue-700 transition duration-700 ease-in-out hover:text-blue-600" />
+            <Logo className="h-24 w-auto text-blue-700 transition duration-700 ease-in-out hover:text-blue-600" />
           </Link>
         </div>
-        <div className="flex flex-auto order-first py-4 -mx-4 overflow-x-auto font-mono text-sm text-blue-600 border-b basis-full whitespace-nowrap border-blue-600/10 sm:-mx-6 lg:order-none lg:mx-0 lg:basis-auto lg:border-0 lg:py-0">
-          <div className="flex items-center gap-4 px-4 mx-auto">
+        <div className="order-first -mx-4 flex flex-auto basis-full overflow-x-auto whitespace-nowrap border-b border-blue-600/10 py-4 font-mono text-sm text-blue-600 sm:-mx-6 lg:order-none lg:mx-0 lg:basis-auto lg:border-0 lg:py-0">
+          <div className="mx-auto flex items-center gap-4 px-4">
             <p>
               <time dateTime="2023-11-11">11</time>-
               <time dateTime="2023-11-12">12 of November 2023</time>
@@ -27,16 +31,15 @@ export function Header() {
           </div>
         </div>
         <div className="hidden sm:flex lg:justify-center">
-          <CalendarButton />
-
+          <CalendarButtonWithNoSSR />
         </div>
       </Container>
-      <div className="px-0 mx-auto sm:px-4 lg:px-8">
+      <div className="mx-auto px-0 sm:px-4 lg:px-8">
         <nav
-          className="flex items-center justify-center mt-6"
+          className="mt-6 flex items-center justify-center"
           aria-label="Site"
         >
-          <ul className="flex items-center justify-center gap-8 px-8 py-4 overflow-x-auto text-lg leading-tight text-center text-blue-900 bg-blue-100 border-b border-blue-900/10 sm:-mx-6 sm:rounded-full lg:mx-0 lg:justify-start">
+          <ul className="flex items-center justify-center gap-8 overflow-x-auto border-b border-blue-900/10 bg-blue-100 px-8 py-4 text-center text-lg leading-tight text-blue-900 sm:-mx-6 sm:rounded-full lg:mx-0 lg:justify-start">
             <li>
               <Link
                 className="hover:underline hover:underline-offset-4 focus:font-bold focus:underline focus:underline-offset-4"
