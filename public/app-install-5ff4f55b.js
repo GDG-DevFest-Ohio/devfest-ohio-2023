d617994a42b21e24624fc0015959be54e36bfd3a
Vagish Vela <git@vagish.com>
2023-09-04 05:50:29 +0000

    fix: remove old service workers and reload the page for all domains

--- public/app-install-5ff4f55b.js
+++ public/app-install-5ff4f55b.js
@@ -4,10 +4,20 @@ if ('serviceWorker' in navigator) {
   navigator.serviceWorker
     .getRegistrations()
     .then(function (registrations) {
+      let unregisterPromises = [] // array to store all the unregister promises
+
       for (let registration of registrations) {
         //unregister service worker for old domain
-        registration.unregister()
+        let unregisterPromise = registration.unregister()
+        unregisterPromises.push(unregisterPromise)
       }
+
+      // Wait for all the unregister promises to complete
+      return Promise.all(unregisterPromises)
+    })
+    .then(function () {
+      // Reload the page once all service workers have been unregistered
+      window.location.reload()
     })
     .catch(function (err) {
       console.log('Fail: ', err)
