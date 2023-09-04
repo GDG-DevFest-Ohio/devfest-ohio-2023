9a8d9a38f77df3f5f4abf4ecaa96ef4adb489f77
Vagish Vela <git@vagish.com>
2023-09-04 04:02:36 +0000

    Replace old service workers post-migration
    Removed legacy service workers from the previous gdg-x/hoverboard setup
    and introduced new ones in different locations based on the old site.
    This change aims to rectify issues causing disruptions on the new
    deployed website.
    
    The prior service workers from the old hoverboard website were
    interfering with the new deployment. This commit seeks to bring
    stability back to the site by ensuring the new service workers are
    aligned with the updated website architecture.
    
    Resolves: #26

---
+++ public/app-install-5ff4f55b.js
@@ -0,0 +1,15 @@
+// contents of service-worker.js
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .getRegistrations()
+    .then(function (registrations) {
+      for (let registration of registrations) {
+        //unregister service worker for old domain
+        registration.unregister()
+      }
+    })
+    .catch(function (err) {
+      console.log('Fail: ', err)
+    })
+}
