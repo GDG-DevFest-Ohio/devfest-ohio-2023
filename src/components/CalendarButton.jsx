29521f59fe1abd5329f1f479d60dd437d83381b1
Vagish Vela <git@vagish.com>
2023-09-04 02:53:06 +0000

    fix prettier issues

--- src/components/CalendarButton.jsx
+++ src/components/CalendarButton.jsx
@@ -15,7 +15,7 @@ export default function CalendarButton() {
       trigger="click"
       label="Save the Date!"
       lightMode="bodyScheme"
-      className="px-4 py-2 font-bold text-blue-600 bg-blue-600 rounded hover:bg-blue-700"
+      className="rounded bg-blue-600 px-4 py-2 font-bold text-blue-600 hover:bg-blue-700"
     ></AddToCalendarButton>
   )
 }
