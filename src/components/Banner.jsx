90cf4e14bd49399100d135be4caacc9b0f4e25e2
Greg Miller <hello@gregmiller.io>
2023-10-20 13:56:44 -0400

    Update Banner.jsx
    Remove save the date button

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,13 +1,13 @@
-import dynamic from 'next/dynamic'
-const CalendarButtonWithNoSSR = dynamic(
-  () => import('@/components/CalendarButton'),
-  { ssr: false },
-)
+//import dynamic from 'next/dynamic'
+// const CalendarButtonWithNoSSR = dynamic(
+//   () => import('@/components/CalendarButton'),
+//   { ssr: false },
+// )
 
 export default function Banner() {
   return (
     <div className="flex items-center justify-center gap-x-6 rounded-lg  px-4 py-2.5 text-center sm:hidden  sm:px-3.5 sm:px-6">
-      <CalendarButtonWithNoSSR />
+      {/* <CalendarButtonWithNoSSR /> */}
     </div>
   )
 }
