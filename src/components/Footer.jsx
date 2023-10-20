f3b458136a4b61f58cd446b1608205c1c3d7ae47
Greg Miller <hello@gregmiller.io>
2023-10-20 13:54:45 -0400

    Update Footer.jsx
    Remove save the date button

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,24 @@
-import dynamic from 'next/dynamic'
+//import dynamic from 'next/dynamic'
 import { Container } from '@/components/Container'
 import { Logo } from '@/components/LogoFooter'
 // import CalendarButton from '@/components/CalendarButton'
-const CalendarButtonWithNoSSR = dynamic(
-  () => import('@/components/CalendarButton'),
-  { ssr: false },
-)
+// const CalendarButtonWithNoSSR = dynamic(
+//   () => import('@/components/CalendarButton'),
+//   { ssr: false },
+// )
 
 export function Footer() {
   return (
     <footer role="contentinfo" className="flex-none py-16">
-      <div className="mb-12 flex items-center justify-center">
-        <CalendarButtonWithNoSSR />
+      <div className="flex items-center justify-center mb-12">
+        {/* <CalendarButtonWithNoSSR /> */}
       </div>
       <Container className="flex flex-row">
         <div className="flex">
-          <Logo className="h-20 w-auto text-blue-600" />
+          <Logo className="w-auto h-20 text-blue-600" />
         </div>
 
-        <div className="mx-auto flex flex-row items-end text-center">
+        <div className="flex flex-row items-end mx-auto text-center">
           <p className="mt-6 text-base text-slate-500 md:mt-0">
             Copyright &copy; {new Date().getFullYear()} Shrinkray Labs. All
             rights reserved.
