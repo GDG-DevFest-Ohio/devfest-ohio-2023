18634a5e028b42dc1f2e6fdfd3a822fd74e6ba64
Greg Miller <hello@gregmiller.io>
2023-11-28 15:07:33 -0500

    Update Header.jsx
    - Clean up code
    - Fix logo in mobile
    - Wrap layout in mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,6 @@
 import Link from 'next/link'
-// import dynamic from 'next/dynamic'
 import { Container } from '@/components/Container'
-//import { DiamondIcon } from '@/components/DiamondIcon'
 import { Logo } from '@/components/Logo'
-// import { HeaderBackground } from './HeaderBackground'
 import  { HeaderBackground }  from '@/components/HeaderBackground'
 // const CalendarButtonWithNoSSR = dynamic(
 //   () => import('@/components/CalendarButton'),
@@ -13,26 +10,21 @@ import  { HeaderBackground }  from '@/components/HeaderBackground'
 export function Header() {
   return (
     <>
-      <header className="relative flex bg-indigo-100 h-60">
-        <Container className="container grid">
-          <div className="z-10 grid content-center grid-cols-3 grid-rows-1 gap-4 justify-items-center">
-            <Link href="/" className="z-10 grid content-center col-start-1 justify-self-end">
-              <Logo className="z-10 w-auto h-32 text-blue-700 transition duration-700 ease-in-out hover:text-blue-600" />
+      <header className="relative z-50 flex flex-none w-full h-72 md:h-60 lg:pt-11">
+        <Container className="container flex md:grid">
+          <div className="z-10 flex flex-wrap content-center justify-center flex-1 grid-rows-2 gap-4 md:grid md:grid-cols-3 md:grid-rows-1">
+            <Link
+              href="/"
+              className="z-10 content-center col-start-1 justify-self-center "
+            >
+              <Logo className="z-10 w-auto h-32 text-red-700 transition duration-700 ease-in-out sm:h-24 header-lg hover:rotate-12 hover:text-blue-600 md:h-44" />
             </Link>
-            <div className="z-10 grid content-center col-start-2 px-0 mx-auto justify-seif-center sm:px-4 lg:px-8">
+            <div className="z-10 grid content-center col-start-2 px-0 mx-auto justify-self-center sm:px-4 lg:px-8">
               <nav
                 className="z-10 flex items-center justify-center mt-6"
                 aria-label="Site"
               >
-                <ul className="flex items-center justify-center gap-8 px-8 py-4 overflow-x-auto text-lg leading-tight text-center text-blue-900 bg-blue-100 border-b z-2 border-blue-900/10 sm:-mx-6 sm:rounded-full lg:mx-0 lg:justify-start">
-                  <li>
-                    <Link
-                      className="hover:underline hover:underline-offset-4 focus:font-bold focus:underline focus:underline-offset-4"
-                      href={'/'}
-                    >
-                      Home
-                    </Link>
-                  </li>
+                <ul className="flex items-center justify-center gap-8 px-8 py-4 overflow-x-auto text-lg leading-tight text-center text-blue-900 bg-white border-4 border-red-700 border-solid z-2 border-blue-900/10 sm:-mx-6 sm:rounded-full lg:mx-0 lg:justify-start">
                   <li>
                     <Link
                       className="hover:underline hover:underline-offset-4 focus:font-bold focus:underline focus:underline-offset-4"
@@ -54,10 +46,8 @@ export function Header() {
             </div>
           </div>
 
-          
-          <HeaderBackground className="z-0" /> 
+          <HeaderBackground className="z-0 w-full bg-no-repeat" />
         </Container>
-        
       </header>
     </>
   )
