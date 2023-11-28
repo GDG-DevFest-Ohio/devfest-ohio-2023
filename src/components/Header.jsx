d64a509e6ae5163639495391a9adc035b58ca2c1
Greg Miller <hello@gregmiller.io>
2023-11-27 21:54:22 -0500

    Update Header.jsx
    Rework Tailwind styles for header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,49 +13,51 @@ import  { HeaderBackground }  from '@/components/HeaderBackground'
 export function Header() {
   return (
     <>
-      <header className="relative flex h-60">
-        <Container className="z-10 flex flex-wrap items-center justify-center">
-          <div className="z-10 mt-10 lg:mt-0">
-            <Link href="/">
-              <Logo className="z-10 w-auto h-24 text-blue-700 transition duration-700 ease-in-out hover:text-blue-600" />
+      <header className="relative flex bg-indigo-100 h-60">
+        <Container className="container grid">
+          <div className="z-10 grid content-center grid-cols-3 grid-rows-1 gap-4 justify-items-center">
+            <Link href="/" className="z-10 grid content-center col-start-1 justify-self-end">
+              <Logo className="z-10 w-auto h-32 text-blue-700 transition duration-700 ease-in-out hover:text-blue-600" />
             </Link>
+            <div className="z-10 grid content-center col-start-2 px-0 mx-auto justify-seif-center sm:px-4 lg:px-8">
+              <nav
+                className="z-10 flex items-center justify-center mt-6"
+                aria-label="Site"
+              >
+                <ul className="flex items-center justify-center gap-8 px-8 py-4 overflow-x-auto text-lg leading-tight text-center text-blue-900 bg-blue-100 border-b z-2 border-blue-900/10 sm:-mx-6 sm:rounded-full lg:mx-0 lg:justify-start">
+                  <li>
+                    <Link
+                      className="hover:underline hover:underline-offset-4 focus:font-bold focus:underline focus:underline-offset-4"
+                      href={'/'}
+                    >
+                      Home
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className="hover:underline hover:underline-offset-4 focus:font-bold focus:underline focus:underline-offset-4"
+                      href={'/become-sponsor'}
+                    >
+                      Event sponsorship
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className="hover:underline hover:underline-offset-4 focus:font-bold focus:underline focus:underline-offset-4"
+                      href={'/get-tickets'}
+                    >
+                      Ticket Info
+                    </Link>
+                  </li>
+                </ul>
+              </nav>
+            </div>
           </div>
 
-          <div className="z-10 px-0 mx-auto align-middle sm:px-4 lg:px-8">
-            <nav
-              className="z-10 flex items-center justify-center mt-6"
-              aria-label="Site"
-            >
-              <ul className="flex items-center justify-center gap-8 px-8 py-4 overflow-x-auto text-lg leading-tight text-center text-blue-900 bg-blue-100 border-b z-2 border-blue-900/10 sm:-mx-6 sm:rounded-full lg:mx-0 lg:justify-start">
-                <li>
-                  <Link
-                    className="hover:underline hover:underline-offset-4 focus:font-bold focus:underline focus:underline-offset-4"
-                    href={'/'}
-                  >
-                    Home
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    className="hover:underline hover:underline-offset-4 focus:font-bold focus:underline focus:underline-offset-4"
-                    href={'/become-sponsor'}
-                  >
-                    Event sponsorship
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    className="hover:underline hover:underline-offset-4 focus:font-bold focus:underline focus:underline-offset-4"
-                    href={'/get-tickets'}
-                  >
-                    Ticket Info
-                  </Link>
-                </li>
-              </ul>
-            </nav>
-          </div>
-          <HeaderBackground className="z-0" />
+          
+          <HeaderBackground className="z-0" /> 
         </Container>
+        
       </header>
     </>
   )
